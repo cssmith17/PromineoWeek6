8aad347bfc04542be76dc2c3f3d342e0ea9d3601
Clayton Smith <claytonssmith@hotmail.com>
2022-11-08 21:20:59 -0600

    commented code

--- CodingAssignment.js
+++ CodingAssignment.js
@@ -1,10 +1,10 @@
 // WAR!
 
-const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
-const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
-const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];    // Declared constant variables containing information to create cards with
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-class Card {
+class Card {    // Card class that creates an object with a card Value or name, card suit, and rank of card to compare against other cards in the game
     constructor(cardValue, cardSuit, cardRank) {
         this.cardValue = cardValue;
         this.cardSuit = cardSuit;
@@ -15,18 +15,18 @@ class Card {
 
 
 
-class Deck {
+class Deck {    // Deck class that assembles 52 Card objects into an array, contains them in the  .cards property
     constructor() {
     this.cards  = [];
 
-        for (let suit in suits) {
+        for (let suit in suits) {   // two for in loops that iterate through all permutations of card values, suits, and ranks, then pushes each card object to this.cards
             for (let rank in ranks) {
                 this.cards.push(new Card(values[rank], suits[suit], ranks[rank]));
             }
         }
     }
 
-    shuffleDeck() {
+    shuffleDeck() {     // function that takes two random indexes and swaps the card objects in each position, 1000 times to shuffle deck
         for (let i = 0; i < 1000; i++) {
             let x = Math.floor(Math.random() * this.cards.length);
             let y = Math.floor(Math.random() * this.cards.length);
@@ -36,22 +36,22 @@ class Deck {
             this.cards[y] = temporary;
         }
     }
-}
+} 
 
-class Player {
+class Player {      // Player class that is used to store each player's deck and their score
     constructor() {
         this.playerHand = [];
         this.playerScore = 0;
     }
 }
 
-const gameDeck = new Deck();
+const gameDeck = new Deck();    // creating a new Deck and shuffling it
 gameDeck.shuffleDeck();
 
-const player1 = new Player();
+const player1 = new Player();   // creating two new Players, named player1 and player2
 const player2 = new Player();
 
-function splitDeck(gameDeck){
+function splitDeck(gameDeck){   // function to split the deck array equally and put half of the card objects into player one's hand, and the other half in player two's hand
     let halfDeck = Math.ceil(gameDeck.cards.length / 2); 
 player1.playerHand = gameDeck.cards.slice(0, halfDeck);
 player2.playerHand = gameDeck.cards.slice(-halfDeck);
@@ -60,21 +60,19 @@ player2.playerHand = gameDeck.cards.slice(-halfDeck);
 }
 splitDeck(gameDeck);
 
-console.log(player1.playerHand)
-
 console.log('<---- WAR! ---->');
 
-for (i = 0; i < 26; i++) {
+for (i = 0; i < 26; i++) {      // for loop that iterates through each turn of the game, for every card
 
-    const playerOneCard = player1.playerHand.pop();
+    const playerOneCard = player1.playerHand.pop();    // Each turn, a card object is removed from each player's hand and stored in constant variables
     const playerTwoCard = player2.playerHand.pop();
 
     console.log(`
     <---- Round ${(i + 1) } ---->
     Player One's Card: ${playerOneCard.cardValue} of ${playerOneCard.cardSuit}
     VS.
-    Player Two's Card: ${playerTwoCard.cardValue} of ${playerTwoCard.cardSuit}`);
-        if (playerOneCard.cardRank > playerTwoCard.cardRank) {
+    Player Two's Card: ${playerTwoCard.cardValue} of ${playerTwoCard.cardSuit}`);   // Compares each players card, via card value and suit
+        if (playerOneCard.cardRank > playerTwoCard.cardRank) {  // If, if else, and else statements to determine who to give points to depending on who has the winning card
             player1.playerScore++;
             console.log(`
     <---- Player One Wins Round ${(i + 1)}! ---->
@@ -95,7 +93,7 @@ for (i = 0; i < 26; i++) {
         }
 }
 console.log(`<---- Game end! ---->
-FINAL SCORE - Player One: ${player1.playerScore} VS. Player Two: ${player2.playerScore}`);
+FINAL SCORE - Player One: ${player1.playerScore} VS. Player Two: ${player2.playerScore}`);  //Displays final scores, then declares a winner for whoever has most points
 if (player1.playerScore > player2.playerScore) {
     console.log(`Player One Wins!`);
 } else if (player1.playerScore < player2.playerScore) {
